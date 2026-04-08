bf4222487d70e223e27948fbe3eb12a272517e80
Sam Q <saminm.qureshi@gmail.com>
2026-04-08 17:07:57 -0400

    Add app entry point with Core Data environment injection

---
+++ A2_iOS_Samin_101534641/A2_iOS_Samin_101534641App.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct A2_iOS_Samin_101534641App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
